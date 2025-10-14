4ea3d7715c94e9c691bfe062970b587847bcdf4f
kirti sengar <kirtisengar95@gmail.com>
2025-10-14 22:19:43 +0530

    Implement user authentication backend with controllers and routes

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 export const signUp = async (req, res) => {
     try {
         const { userName, email, password } = req.body;
@@ -14,7 +15,56 @@ export const signUp = async (req, res) => {
         if(password.length < 6){
             return res.status(400).json({ message: "Password must be at least 6 characters long" });
         }
+
+        const hashedPassword=await bcrypt.hash(password,10);
+        const user = await User.create({
+            userName,
+            email,
+            password:hashedPassword
+        })
+        
+        const token = generateToken(user._id);
+        res.cookie("token", token, {
+            httpOnly: true,
+            maxAge:7 * 24 * 60 * 60 * 1000,
+            sameSite:"None",
+            secure:false,
+        })
+
+        return res.status(201).json(user)
+
+
     } catch (error) {
         res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "user doesn't exists" });
+        }
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Incorrect password" });
+        }
+        
+        const token = await genToken(user._id);
+        res.cookie("token", token, {
+            httpOnly: true,
+            maxAge:7 * 24 * 60 * 60 * 1000,
+            sameSite:"None",
+            secure:false,
+        })
+
+        return res.status(200).json(user)
+
+
+    } catch (error) {
+        res.status(500).json({ message: "login error" });
     }
 }

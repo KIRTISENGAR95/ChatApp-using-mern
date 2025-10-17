0ddd84ae5aca9813d04ce462e37c120638f48aa7
kirti sengar <kirtisengar95@gmail.com>
2025-10-17 21:32:18 +0530

    Resolve form submission and styling issues in SignUp component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,39 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
+import axios from 'axios'
+// import { serverUrl } from '../config'
 function Login(){
   let navigate = useNavigate()
   let [show, setShow] = useState(false)
+  let [email,setEmail]=useState("")
+  let [password,setPassword]=useState("")
+
+  const handleLogin=async(e)=>{
+    e.preventDefault()
+    try{
+      let result=await axios.post(`${serverUrl}/api/auth/login`,{
+        email,
+        password
+      },{withCredentials:true})
+      console.log(result)
+      setEmail("")
+      setPassword("")
+    }catch(error){
+      console.log(error)
+    }
+  }
   return (
     <div className='w-full h-[100vh] bg-slate-300 flex items-center justify-center'>
       <div className='w-full max-w-[500px] h-[600px] bg-white rounded-lg shadow-gray-400 shadow-lg flex flex-col gap-[10px]'>
         <div className='w-full h-[200px] bg-[#19cdff] rounded-b-[30%] shadow-gray-400 shadow-lg flex items-center justify-center'>
           <h1 className='text-gray-600 font-bold text-[30px]'>Login to <span className='text-white'> ChatApp</span></h1>
         </div>
-        <form className='w-full flex flex-col gap-[20px] items-center'>
+        <form className='w-full flex flex-col gap-[20px] items-center' onSubmit={handleLogin}>
 
-        
-        <input type="email" placeholder='email' className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-700 text-[19px]'/>
+        <input type="email" placeholder='email' className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-700 text-[19px]' onChange={(e)=>setEmail(e.target.value)} value={email}/>
 
         <div className='w-[90%] h-[50px] border-2 border-[#20c7ff] overflow-hidden rounded-lg shadow-gray-200 shadow-lg relative'>
-          <input type={`${show ? "text":"password"}`} placeholder='password' className='w-full h-full outline-none px-[20px] py-[10px] bg-[white] shadow-gray-200 shadow-lg text-gray-700 text-[19px]'/>
+          <input type={`${show ? "text":"password"}`} placeholder='password' className='w-full h-full outline-none px-[20px] py-[10px] bg-[white] shadow-gray-200 shadow-lg text-gray-700 text-[19px]'onChange={(e)=>setPassword(e.target.value)} value={password}/>
           <span className='absolute top-[10px] right-[20px] text-[19px] text-[#20c7ff] font-semibold cursor-pointer' onClick={()=>setShow(prev=>!prev )}>{`${show?"hidden":"show"}`}</span>
         </div>
 
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,25 +1,50 @@
 import React, { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import { serverUrl } from '../main'
 function SignUp(){
   let navigate = useNavigate()
   let [show, setShow] = useState(false)
+
+  let [userName,setUserName]=useState("")
+  let [email,setEmail]=useState("")
+  let [password,setPassword]=useState("")
+  let[loading,setLoading]=useState(false)
+
+  const handleSignUp=async(e)=>{
+    e.preventDefault()
+    setLoading(true)
+    try{
+      let result=await axios.post(`${serverUrl}/api/auth/signup`,{
+        userName,
+        email,
+        password
+      },{withCredentials:true})
+      console.log(result)
+      setEmail("")
+      setPassword("")
+      setLoading(false)
+    }catch(error){
+      console.log(error)
+      setLoading(false)
+    }
+  }
   return (
     <div className='w-full h-[100vh] bg-slate-300 flex items-center justify-center'>
       <div className='w-full max-w-[500px] h-[600px] bg-white rounded-lg shadow-gray-400 shadow-lg flex flex-col gap-[10px]'>
         <div className='w-full h-[200px] bg-[#19cdff] rounded-b-[30%] shadow-gray-400 shadow-lg flex items-center justify-center'>
           <h1 className='text-gray-600 font-bold text-[30px]'>welcome to <span className='text-white'> ChatApp</span></h1>
         </div>
-        <form className='w-full flex flex-col gap-[20px] items-center'>
+        <form className='w-full flex flex-col gap-[20px] items-center' onSubmit={handleSignUp}>
 
-        <input type="text" placeholder='username' className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-700 text-[19px]'/>
-        <input type="email" placeholder='email' className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-700 text-[19px]'/>
-
+        <input type="text" placeholder='username' className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-700 text-[19px]' onChange={(e)=>setUserName(e.target.value)} value={userName}/>
+        <input type="email" placeholder='email' className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-700 text-[19px]' onChange={(e)=>setEmail(e.target.value)} value={email}/>
         <div className='w-[90%] h-[50px] border-2 border-[#20c7ff] overflow-hidden rounded-lg shadow-gray-200 shadow-lg relative'>
-          <input type={`${show ? "text":"password"}`} placeholder='password' className='w-full h-full outline-none px-[20px] py-[10px] bg-[white] shadow-gray-200 shadow-lg text-gray-700 text-[19px]'/>
+          <input type={`${show ? "text":"password"}`} placeholder='password' className='w-full h-full outline-none px-[20px] py-[10px] bg-[white] shadow-gray-200 shadow-lg text-gray-700 text-[19px]'onChange={(e)=>setPassword(e.target.value)} value={password}/>
           <span className='absolute top-[10px] right-[20px] text-[19px] text-[#20c7ff] font-semibold cursor-pointer' onClick={()=>setShow(prev=>!prev )}>{`${show?"hidden":"show"}`}</span>
         </div>
 
-        <button className='px-[20px] py-10px] bg-[#20c7ff] rounded-2xl shadow-gray-400 shadow-lg text-[20px] w-[200px] mt-[20px] font-semibold hover:shadow-inner'>Sign Up</button>
+        <button type="submit" className='px-[20px] py-[10px] bg-[#20c7ff] rounded-2xl shadow-gray-400 shadow-lg text-[20px] w-[200px] mt-[20px] font-semibold hover:shadow-inner'>{loading?"Loading...":" Sign Up"}</button>
 
         <p className='cursor-pointer' onClick={()=>navigate("/login")}>Already have an Account ? <span className='text-[#20c7ff] text-[bold]'>Login</span></p>
       </form>

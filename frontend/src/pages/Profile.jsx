5f7aaf05838beba5e1547041598a5212d9472b99
kirti sengar <kirtisengar95@gmail.com>
2025-10-19 22:08:36 +0530

    Added Profile component with user info form and avatar section

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
-
+import dp from "../assets/dp.webp"
+import { IoIosCamera } from "react-icons/io";
 function Profile(){
+    let {userData} = useSelector(state=>state.user)
     return(
-        <div className='w-full h-[100vh] bg-slate-200 flex flex-col items-center justify-center'>
-            <div className='w-[200px] h-[200px] bg-white rounded-full border-2 border-[#20c7ff]'>
-
+        <div className='w-full h-[100vh] bg-slate-200 flex flex-col items-center justify-center gap-[20px]'>
+            <div className='bg-white rounded-full border-4 border-[#20c7ff] shadow-gray-400 shadow-lg relative'>
+                <div className='w-[200px] h-[200px] rounded-full overflow-hidden'>
+                    <img src={dp} alt="" className='h-[100%]'/>
+                </div>
+                <IoIosCamera className='absolute bottom-4 text-gray-700 right-5 w-[28px] h-[28px]'/>
             </div>
-            <form action=""></form>
+            <form className='w-[95%] h-[600px] max-w-[500px] flex flex-col gap-[20px] items-center justify-center'>
+                <input type="text" placeholder="Enter your name">
+                className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-700 text-[19px]'
+                </input>
+                <input type="text" readOnly className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-400 text-[19px]' value={userData.userName}></input>
+                <input type="email" readOnly className='w-[90%] h-[50px] outline-none border-2 border-[#20c7ff] px-[20px] py-[10px] bg-[white] rounded-lg shadow-gray-200 shadow-lg text-gray-400 text-[19px]' value={userData?.email}></input>
+                <button>Save profile</button>
+            </form>
+            
         </div>
     )
 }

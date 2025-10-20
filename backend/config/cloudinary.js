2e964b7d24323fce53fd2169cc3f7930bad2a0ef
kirti sengar <kirtisengar95@gmail.com>
2025-10-20 13:50:38 +0530

    Added Cloudinary upload utility with initial configuration setup

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,5 +1,15 @@
 import { v2 as cloudinary} from "cloudinary";
 
 const uploadOnCloudinary = async(filePath)=>{
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.API_KEY, 
+    api_secret: process.env.API_SECRET
+})
 
+try{
+
+}catch(error){
+    
+}
 }

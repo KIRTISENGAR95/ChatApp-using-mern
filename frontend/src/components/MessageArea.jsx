e073e287684a25591bf5c0b6f8e682d2a572dfcd
kirti sengar <kirtisengar95@gmail.com>
2025-11-05 16:06:31 +0530

    Add emoji picker integration in chat input

--- frontend/src/components/MessageArea.jsx
+++ frontend/src/components/MessageArea.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosArrowRoundBack } from "react-icons/io";
 import dp from "../assets/dp.webp";
 import { useSelector, useDispatch } from "react-redux";
@@ -6,13 +6,17 @@ import { setSelectedUser } from "../redux/userSlice";
 import {RiEmojiStickerLine} from "react-icons/ri"
 import {FaImages} from "react-icons/fa6"
 import {RiSendPlane2Fill} from "react-icons/ri"
+import EmojiPicker from 'emoji-picker-react'
 
 function MessageArea() {
   let {selectedUser}=useSelector(state=>state.user);
   let dispatch=useDispatch();
+  let[showPicker,setShowPicker]=useState(false);
   return (
     <div className={`lg:w-[70%] relative ${selectedUser?"flex":"hidden"} lg:flex w-full h-full bg-slate-200 border-l-2 border-gray-300`}>
-      {selectedUser && <div className='w-full h-[100px] bg-[#1797c2] shadow-gray-400 shadow-lg gap-[20px] flex items-center px-[20px]'>
+      {selectedUser && 
+      <div className='w-full h-[100vh] flex flex-col'>
+      <div className='w-full h-[100px] bg-[#1797c2] shadow-gray-400 shadow-lg gap-[20px] flex flex-col items-center px-[20px]'>
         <div className="flex items-center gap-4 mt-[20px]">
           <div className="cursor-pointer" onClick={()=>dispatch(setSelectedUser(null))}>
             <IoIosArrowRoundBack className="w-[40px] h-[40px] text-white" />
@@ -25,18 +29,27 @@ function MessageArea() {
             <h1 className="text-white font-semibold text-[20px]">{selectedUser?.name || "user"}</h1>
           </div>
         </div>
-      </div>}
+      </div>
+
+      <div className='w-full h-[550px]'>
+        {showPicker && <EmojiPicker/> }
+
+      </div>
+      </div>
+      }
+
+
       {!selectedUser && <div className='w-full h-full flex  flex-col justify-center items-center '>
         <h1 className='text-gray-700 font-bold text-[50px]'>welcome to Talkify</h1>
         <span className='text-gray-700 font-semibold text-[30px]'>Talk with Friendly !</span>
       </div>}
       
       {selectedUser && <div className='w-full lg:w-[70%] h-[100px] fixed bottom-[20px] flex items-center justify-center marker:'>
-        <form className='w-[95%] max-w-[70%] h-[60px] bg-[#1797c2] shadow-gray-400 shadow-lg rounded-full flex items-center gap-[20px] px-[20px]'>
-          <div>
+        <form className='w-[95%] lg:w-[70%] h-[60px] bg-[#1797c2] shadow-gray-400 shadow-lg rounded-full flex items-center gap-[20px] px-[20px]' onSubmit={(e)=>e.preventDefault()}>
+          <div onClick={()=>setShowPicker(prev=>!prev)}>
             <RiEmojiStickerLine className='w-[25px] h-[25px] text-white cursor-pointer'/>
           </div>
-          <input type="text" className='w-full h-full px-[10px] outline-none border-0 text-[19px] text-white bg-transparent'/>
+          <input type="text" className='w-full h-full px-[10px] outline-none border-0 text-[19px] text-white bg-transparent placeholder-white' placeholder='Message'/>
           <div >
             <FaImages className='w-[25px] h-[25px] cursor-pointer text-white'/>
           </div>

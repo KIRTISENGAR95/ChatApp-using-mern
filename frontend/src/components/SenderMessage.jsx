c0d08ba6a49258e1f9dafd8c424e4653eabf207e
kirti sengar <kirtisengar95@gmail.com>
2025-11-12 21:29:27 +0530

    realtime chats

--- frontend/src/components/SenderMessage.jsx
+++ frontend/src/components/SenderMessage.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import dp from "../assets/dp.webp"
 function SenderMessage({ image,message }) {
+  let scroll=useRef();
+  useEffect(()=>{
+    scroll?.current.scrollIntoView({behavior:"smooth"});
+  },[message,image])
   return (
-    <div className='w-fit max-w-[500px] px-[20px] py-[10px] bg-[rgb(23,151,194)] text-white text-[19px] rounded-tr-none rounded-2xl relative right-0 ml-auto shadow-gray-400 shadow-lg gap-[10px] flex flex-col'>
+    <div className='w-fit max-w-[500px] px-[20px] py-[10px] bg-[rgb(23,151,194)] text-white text-[19px] rounded-tr-none rounded-2xl relative right-0 ml-auto shadow-gray-400 shadow-lg gap-[10px] flex flex-col'ref={scroll}>
        {image &&  <img src={image} alt="" className='w-[150px] rounded-lg'></img>}
-        {message && <span>{message}</span>}
+        {message && <span >{message}</span>}
     
     </div>
   );
